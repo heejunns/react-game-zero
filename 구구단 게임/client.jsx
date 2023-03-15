e6a9309367537e51f04e65199de6a83c15c01ed7
heejunns <wns2252@hanmail.net>
2023-03-15 20:44:56 +0900

    구구단 client.jsx 파일

--- 구구단 게임/client.jsx
+++ 구구단 게임/client.jsx
@@ -1,4 +1,5 @@
 const React = require('react'); // react 와 reactdom 을 불러온다. 노드의 모듈 시스템을 이용
 const ReactDom = require('react-dom');
 const GuGuDan = require('./GuGuDan');
+
 ReactDom.render(<GuGuDan />, document.querySelector('#root'));
